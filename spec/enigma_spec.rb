940f253bf6e4ab7585f781690aa8c1886093d8be
Austin Moore <austinchristianmoore@gmail.com>
2022-01-14 12:49:35 -0500

    Feature: Create tests with mocks

---
+++ spec/enigma_spec.rb
@@ -0,0 +1,75 @@
+require 'date'
+require './lib/enigma'
+
+RSpec.describe Enigma do
+  before (:each) do
+    @enigma = Enigma.new
+    @key    = double("02715")
+    @today  = double("040895")
+  end
+
+  it "exists" do
+    expect(@enigma).to be_instance_of Enigma
+  end
+
+  it "can encrypt a message with a key and date" do
+    expect(@enigma.encrypt("hello world", "02715", "040895")).to be_instance_of Hash
+    expect(@enigma.encrypt("hello world", "02715", "040895")).to eq(
+      {
+        encryption: "keder ohulw",
+        key: "02715",
+        date: "040895"
+      })
+  end
+
+  it "can decrypt a message with a key and date" do
+    expect(@enigma.decrypt("keder ohulw", "02715", "040895")).to be_instance_of Hash
+    expect(@enigma.decrypt("keder ohulw", "02715", "040895")).to eq(
+      {
+        decryption: "hello world",
+        key: "02715",
+        date: "040895"
+      })
+  end
+
+  it "can encrypt a message with a key" do
+    expect(@enigma.encrypt("hello world", "02715")).to eq(@enigma.encrypt(
+        "hello world",
+        "02715",
+        @today
+      )
+    # expect(@enigma.encrypt("hello world", "02715")).to eq(@enigma.encrypt(
+    #   "hello world",
+    #   "02715",
+    #   (Date.today.strftime"%d%m%y").to_i
+    #   ))
+  end
+
+  it "can decrypt a message with a key" do
+    expect(@enigma.decrypt(encrypted[:encryption], "02715")).to be_instance_of Hash
+    expect(@enigma.decrypt(encrypted[:encryption], "02715")).to eq(@enigma.decrypt(
+        encrypted[:encryption],
+        "02715",
+        @today
+      )
+    # expect(@enigma.decrypt(encrypted[:encryption], "02715")).to eq(@enigma.decrypt(
+    #   encrypted[:encryption],
+    #   "02715",
+    #   (Date.today.strftime"%d%m%y").to_i
+    # ))
+  end
+
+  it "can encrypt a message" do
+    expect(@enigma.encrypt("hello world")).to be_instance_of Hash
+    expect(@enigma.encrypt("hello world")).to eq(@enigma.encrypt(
+      "hello world",
+      @key,
+      @today
+      )
+    # expect(@enigma.encrypt("hello world")).to eq(@enigma.encrypt(
+    #   "hello world",
+    #   @enigma.encrypt("hello world")[:key],
+    #   (Date.today.strftime"%d%m%y").to_i
+    #   ))
+  end
+end
