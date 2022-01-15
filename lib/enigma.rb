3da3adbbb03d3ed1be17611d7d8414479ff52856
Austin Moore <austinchristianmoore@gmail.com>
2022-01-15 15:17:45 -0500

    Refactor: Change order of pseudocoded methods

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -8,19 +8,8 @@ class Enigma
   end
 
   def encrypt(message, key = make_key, date = todays_date)
+
     require "pry" ; binding.pry
-    #message
-      #receives a string, downcases it
-      #shifts "valid" characters using sum of the offsets and the keys
-        #for the keys (given 5 digit key as string)
-          #for the A key
-            #take key as string, return 1st and 2nd digits as integer
-          #for the B key
-            #take key as string, return 2nd and 3rd digits as integer
-          #for the C key
-            #take key as string, return 3rd and 4th digits as integer
-          #for the D key
-            #take key as string, return 4th and 5th digits as integer
         #for the offsets (given 6 digit date as integer)
           #square the date
           #take the last 4 digits of that new number
@@ -35,6 +24,18 @@ class Enigma
         #for the shifts (each letter's shift)
           #for each letter, sum each key and offset
           #rotate corresponding letter set that # of positions
+          #message
+        #receives a string, downcases it
+        #shifts "valid" characters using sum of the offsets and the keys
+          #for the keys (given 5 digit key as string)
+            #for the A key
+              #take key as string, return 1st and 2nd digits as integer
+            #for the B key
+              #take key as string, return 2nd and 3rd digits as integer
+            #for the C key
+              #take key as string, return 3rd and 4th digits as integer
+            #for the D key
+              #take key as string, return 4th and 5th digits as integer
         #"validates" characters
           #Create valid letter set, containing all a,b,c...z AND SPACE
             #if character is in letter set
