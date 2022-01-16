d99a7516185afde068545bc1857c46f40172edcf
Austin Moore <austinchristianmoore@gmail.com>
2022-01-15 21:14:41 -0500

    Feature: Create shifts tests

--- spec/caesar_cipher_spec.rb
+++ spec/caesar_cipher_spec.rb
@@ -4,6 +4,8 @@ require './lib/caesar_cipher'
 RSpec.describe CaesarCipher do
   before (:each) do
     @caesar_cipher = CaesarCipher.new("hello world", "02715", "040895")
+    @keys = [2,27,71,15]
+    @offsets = [1,0,2,5]
   end
 
   it "exists" do
@@ -15,7 +17,7 @@ RSpec.describe CaesarCipher do
     expect(CaesarCipher.create_offset("040895")).to eq([1,0,2,5])
   end
 
-  it "returns a qty of digits" do
+  it "splits the offset" do
     expect(CaesarCipher.split_offset(1672401025)).to be_instance_of Array
     expect(CaesarCipher.split_offset(1672401025).length).to eq(4)
     expect(CaesarCipher.split_offset(1672401025)).to eq([1,0,2,5])
@@ -25,5 +27,11 @@ RSpec.describe CaesarCipher do
     expect(CaesarCipher.split_keys("02715")).to be_instance_of Array
     expect(CaesarCipher.split_keys("02715").length).to eq(4)
     expect(CaesarCipher.split_keys("02715")).to eq([2, 27, 71, 15])
+  end
+
+  it "finds the shift" do
+    expect(CaesarCipher.find_shift(@keys, @offsets)).to be_instance_of Array
+    expect(CaesarCipher.find_shift(@keys, @offsets).length).to eq(4)
+    expect(CaesarCipher.find_shift(@keys, @offsets)).to eq([3, 27, 73, 20])
   end
 end
